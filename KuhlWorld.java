6b7ba9a1a129fd4fdf522bbe88ebf5cff1484439
Lochlann Kuhl <35469091+Lochlann-Kuhl@users.noreply.github.com>
2021-11-05 05:30:28 +0000

    I added the outline for the interface, created the KuhlWorld file, and added some descriptions for the various types of humans (including my additional one, the Rogue). Let me know if that can be considered part of Razzle Dazzle

---
+++ KuhlWorld.java
@@ -0,0 +1,1 @@
+/* This is the test program for the code, designated as KuhlWorld (pronounced cool-world). It will be likely the last to be produced, seeing as it requires the rest of the code to use as building blocks.*/
