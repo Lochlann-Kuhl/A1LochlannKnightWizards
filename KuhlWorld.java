6fa96172f81c48cbe1dfae52fd42ba5c12d3d76e
Lochlann Kuhl <35469091+Lochlann-Kuhl@users.noreply.github.com>
2021-11-08 18:12:56 +0000

    I replaced the name Kuhlworld with Calibre, and updated across the code. I also added much more extensive lore via link

--- KuhlWorld.java
+++
@@ -1,1 +0,0 @@
-/* This is the test program for the code, designated as KuhlWorld (pronounced cool-world). It will be likely the last to be produced, seeing as it requires the rest of the code to use as building blocks.*/
