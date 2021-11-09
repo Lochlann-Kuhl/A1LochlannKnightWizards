ad891698eb5fdc2d6457def24ffd2e60484ba99a
Lochlann Kuhl <35469091+Lochlann-Kuhl@users.noreply.github.com>
2021-11-09 18:46:38 +0000

    just added some comments to the main file

--- Calibre.java
+++
@@ -1,8 +0,0 @@
-/* This is the test program for the code, designated as Calibre. It will be likely the last to be produced, seeing as it requires the rest of the code to use as building blocks.*/
-public class Main {
-
-  public static void main(String[] args) {
-    System.out.println("test");
-  }
-
-}
