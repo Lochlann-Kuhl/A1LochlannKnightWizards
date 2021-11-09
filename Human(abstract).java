59bee536199e7601a387b3df993e54ab8d626463
Lochlann Kuhl <35469091+Lochlann-Kuhl@users.noreply.github.com>
2021-11-09 12:45:10 +0000

    Removed the draw class in HumanInterface, added a skeleton on Human(abstract)

---
+++ Human(abstract).java
@@ -0,0 +1,28 @@
+// parent class for the different human types
+public abstract class Human {
+  // All of the public classes
+  public int getNumHumans() {
+
+  }
+  public void setNumHumans() {
+
+  }
+  public String getAgeRules() {
+
+  } 
+  public String getHeightRules() {
+
+  }
+  public String getHealthRules() {
+ 
+  }
+  public void move() {
+
+  }
+  public int getXPos() {
+
+  }
+  public int getYPos() {
+    
+  }
+}
