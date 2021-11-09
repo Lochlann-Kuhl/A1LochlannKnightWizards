56aea23971f4f03bf3b9af190065b33b7f4adf28
Lochlann Kuhl <35469091+Lochlann-Kuhl@users.noreply.github.com>
2021-11-09 18:26:08 +0000

    Edited the replit file, heavily modified the skeleton of human, added some rudimentary additions to the types of humans

---
+++ Knight(concrete).java
@@ -0,0 +1,16 @@
+public class Knight extends Human implements HumanInterface {
+  // Setting the name of the horse
+  private String horseName() {
+
+  }
+  public void setHorse() {
+
+  } 
+  public String getHorseName(String n) {
+
+  } 
+  public boolean equals(Knight k) {
+
+  }
+  
+}
